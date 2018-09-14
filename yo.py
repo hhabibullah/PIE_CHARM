329ad41e9f1787e0a40516522d9180bf780d25a0
hhabibullah <h.habibullah@student.fontys.nl>
2018-09-14 14:28:14 +0200

    second change

--- yo.py
+++ yo.py
@@ -4,6 +4,6 @@ i =0
 
 while i < len(colors):
 
-    print('when i was %d and my favorite color was %s'%(i,colors[i]))
+    print('when i was %d and my favorite color was %s',i,colors[i])
     i+=1
 print('hello world')
