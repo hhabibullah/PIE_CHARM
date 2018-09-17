99bdff647f5d9b4bd78e14a73c7f08e4e95d451a
hhabibullah <h.habibullah@student.fontys.nl>
2018-09-17 18:31:42 +0200

    message

--- Sallam.py
+++ Sallam.py
@@ -6,3 +6,5 @@ print('what is your name')
 
 print('how are you dear with the new changes ')
 print('New changes are needed to be discussed here  ')
+
+print('************************************************')
