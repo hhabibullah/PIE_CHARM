0e8e8345c310bcf52a2883d4cd36b7b04cbf1f7b
hhabibullah <h.habibullah@student.fontys.nl>
2018-09-17 10:17:38 +0200

    add line

--- Hello.py
+++ Hello.py
@@ -6,6 +6,8 @@ while i<10:
     print('Hamid')
     i+=1
 
-print('H')
-print('I')
-print('P')
+print('this is language is awesome to learn it and play with it ')
+
+print('Hello world ')
+
+print('My name is hamid and what is your')
