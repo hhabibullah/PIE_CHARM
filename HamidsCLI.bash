e02a01f6a420a95a01f489bf7eae47c7e73ced43
hhabibullah <h.habibullah@student.fontys.nl>
2018-09-18 18:20:09 +0200

    add title to test.bash

---
+++ HamidsCLI.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to Hamid's Command line Interface"
+
+# Ask the user for their name
+echo Hello, who am I talking to?
+read varname
+echo It\'s nice to meet you $varname
+echo
+echo '**********'
+echo
+
+echo "Goodbye"
